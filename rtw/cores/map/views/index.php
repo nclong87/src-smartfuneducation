7e84fe69140bb837115d929d992ab3fabcd0ad08
nclong87 <nclong87@b818f4ca-a468-9981-6fd0-b9d2f0228eef>
2014-10-05 04:29:07 +0000

    plus_20141003

--- rtw/cores/map/views/index.php
+++ rtw/cores/map/views/index.php
@@ -1,3 +1,5 @@
 <div style="display: block;text-align: center">
-     <img src="<?php echo module_static_url ?>rsz_map" />
+    <a href="/mod/rtw/view.php?id=<?php echo $course->id ?>&c=warmup" class="button">Warm Up</a>
+    <a href="/mod/rtw/view.php?id=<?php echo $course->id ?>&c=quiz" class="button">Quiz</a>
+    <a href="/mod/rtw/view.php?id=<?php echo $course->id ?>&c=videoquiz" class="button">Video Quiz</a>
 </div>
