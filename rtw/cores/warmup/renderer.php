f0017bd41c3b5936d4405009d38c136286b495b7
nclong87 <nclong87@b818f4ca-a468-9981-6fd0-b9d2f0228eef>
2014-10-05 03:20:59 +0000

    plus_20141003

--- rtw/cores/warmup/renderer.php
+++ rtw/cores/warmup/renderer.php
@@ -7,11 +7,18 @@ class mod_rtw_renderer extends mod_rtw_renderer_base {
     
     public function render_index() {
         $this->_file = 'index.php';
+        $test = optional_param('test', '', PARAM_TEXT); 
+        $this->set_var('test', $test);
         $this->doRender();
     }
     
     public function render_add() {
-        \mod_rtw\core\player::getInstance()->change_coin(1, 1);
+        //\mod_rtw\core\player::getInstance()->change_coin(1, 1);
+        $data = array(
+            'player_game_id' => 1,
+            'group_id' => 4
+        );
+        \mod_rtw\db\warmup::getInstance()->insert($data);
         $this->set_var('player_info', \mod_rtw\core\player::getInstance()->getPlayerInfo());
         $this->_file = 'add.php';
         $this->doRender();
