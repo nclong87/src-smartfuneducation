01792cb27e69d74f6b4257ee6dd98cf7882aa04d
nhhieu89 <nhhieu89@gmail.com>
2014-11-06 05:17:27 +0000

    Add new db with unicode support

--- rtw/cores/situation_quiz/db/mdl_rtw_game_situation_quiz.sql
+++ rtw/cores/situation_quiz/db/mdl_rtw_game_situation_quiz.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10.1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 05, 2014 at 04:47 AM
+-- Generation Time: Nov 06, 2014 at 12:15 AM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.4
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `smartfunedu`
+-- Database: `moodle`
 --
 
 -- --------------------------------------------------------
@@ -27,37 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `mdl_rtw_game_situation_quiz` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `game_player_id` int(11) DEFAULT NULL,
   `question_id` int(11) DEFAULT NULL,
   `show_time` datetime DEFAULT NULL,
   `submit_time` datetime DEFAULT NULL,
-  `user_answer` text,
+  `user_answer` text CHARACTER SET utf8 COLLATE utf8_bin,
   `coin_id` int(11) DEFAULT NULL,
   `experience_id` int(11) DEFAULT NULL,
   `start_time` datetime NOT NULL,
-  `self_evaluation` int(1) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+  `self_evaluation` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mdl_rtw_game_situation_quiz`
---
-ALTER TABLE `mdl_rtw_game_situation_quiz`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `mdl_rtw_game_situation_quiz`
---
-ALTER TABLE `mdl_rtw_game_situation_quiz`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=53;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
