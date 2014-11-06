9e977c77e33dccba89c5eccac78f18486330258f
nhhieu89 <nhhieu89@gmail.com>
2014-11-06 03:59:22 +0000

    first init for situation quiz module

---
+++ rtw/cores/situation_quiz/views/intro.php
@@ -0,0 +1,7 @@
+<div id="rtw_content" style="text-align: center">
+    <h1>Situation Quiz Game</h1>     
+    <div style="margin-top: 20px">
+    <a class="button" href="/mod/rtw/view.php?id=<?php echo $course_module->id?>&c=map&a=level&l=<?php echo $player_info->current_level?>">Trở về Map</a>
+    <a style="margin-left: 20px" class="button" href="/mod/rtw/view.php?id=<?php echo $course_module->id?>&c=situation_quiz">Bắt đầu chơi</a>
+    </div>
+</div>
---
+++ rtw/cores/situation_quiz/views/result.php
@@ -0,0 +1,12 @@
+<div style="display: block;text-align: center;padding: 20px">
+     <div id="result-wrapper">
+     <h1>Cảm ơn bạn đã trả lời câu hỏi</h1>
+    <?php if(isset($change_coin)) { ?>
+        <i>Bạn được cộng thêm <b><?php echo $change_coin?></b> xu</i>
+    <?php } ?>     
+    <br clear="all"/>
+    </div>         
+</div>
+<script>
+         //('a.colorbox').colorbox.resize({'width':500, 'height': 600});
+</script>
